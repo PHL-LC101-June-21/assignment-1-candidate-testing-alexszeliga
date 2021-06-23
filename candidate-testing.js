dfcce002388b01c69da49d90afaeaecb7816392a
Alex Szeliga <alexszeliga@gmail.com>
2021-06-23 22:37:47 +0000

    add trailing spaces?

--- candidate-testing.js
+++ candidate-testing.js
@@ -9,11 +9,11 @@ let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 let questions = [
-  "Who was the first American woman in space?",
-  "True or false: 5 kilometer == 5000 meters?",
-  "(5 + 3)/2 * 10 = ?",
-  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?",
-  "What is the minimum crew size for the ISS?"
+  "Who was the first American woman in space? ",
+  "True or false: 5 kilometer == 5000 meters? ",
+  "(5 + 3)/2 * 10 = ? ",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "What is the minimum crew size for the ISS? "
 ];
 let correctAnswers = [
   "Sally Ride",
